5045eb7d0b408c7fced9865ed8f1688f46a74097
jinwei <jinweihuang@gpmcorp.com.tw>
2023-05-04 06:39:38 +0800

    AGVSMsgDisplay  component bug fixed and add only show 0301 Msg function.

--- src/components/AGVSMsgDisplay.vue
+++ src/components/AGVSMsgDisplay.vue
@@ -1,4 +1,10 @@
 <template>
+  <div class="d-flex flex-row my-1">
+    <span class="mx-1 py-1">Only Task Message</span>
+    <el-switch v-model="only_task_msg"></el-switch>
+    <span class="flex-fill"></span>
+    <b-button class size="sm" variant="danger" @click="ClearHandler">Clear</b-button>
+  </div>
   <el-table height="580" :data="MsgDataList" border>
     <el-table-column label="時間" prop="TimeFormated"></el-table-column>
     <el-table-column label="方向" prop="Direction_str">
@@ -19,19 +25,30 @@ import moment from 'moment'
 export default {
   data() {
     return {
-      MsgDataList: []
+      MsgDataList: [],
+      only_task_msg: true
     }
   },
   methods: {
+    ClearHandler() {
+      this.MsgDataList = [];
+    },
     AddNewMsgData(data) {
       var ary = []
+      if (this.only_task_msg) {
+        if (!data.Message.includes('0301')) {
+          return;
+        }
+        var task_msg = JSON.parse(data.Message.replaceAll("*\r", ""))
 
+      }
       if (this.MsgDataList.length >= 20) {
-        this.MsgDataList.removeAt(0)
+        this.MsgDataList.pop();
       }
       data.TimeFormated = moment(data.Time).format('yyyy-MM-DD HH:mm:ss')
       data.Direction_str = data.Direction == 0 ? 'AGVS->VMS' : 'VMS->AGVS'
-      data.Msg_Display = data.Message.substring(0, 150) + '...';
+      // data.Msg_Display = data.Message.substring(0, 150) + '...';
+      data.Msg_Display = data.Message;
       this.MsgDataList.unshift(data);
     }
     ,
